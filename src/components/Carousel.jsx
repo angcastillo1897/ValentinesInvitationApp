138bedabad8e14e963f4c8a614bb0022355ea974
angcastillo1897 <angcastillo18@gmail.com>
2025-02-05 03:48:27 -0500

    chore: change lazyload.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,7 +6,7 @@ import couple1 from "../img/couple1.webp"
 import couple2 from "../img/couple2.webp"
 import couple3 from "../img/couple3.webp"
 const MyCarousel = () => {
-    const settings = { dots: true, infinite: true, speed: 500, slidesToShow: 1,arrows:false, slidesToScroll: 1, autoplay: true, autoplaySpeed: 1500 , lazyLoad:'ondemand' };
+    const settings = { dots: true, infinite: true, speed: 500, slidesToShow: 1,arrows:false, slidesToScroll: 1, autoplay: true, autoplaySpeed: 1500  };
     return (
         <Slider {...settings} className="w-[250px] sm:w-[300px] md:w-[400px]">
         <div className='polaroid'><img className='polaroid-img' src={couple1} alt="couple" /></div>
