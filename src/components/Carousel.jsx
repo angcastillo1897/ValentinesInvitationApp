e5264e883a448be8e5fb1e8a351dd0c7fa7c5cfd
angcastillo1897 <angcastillo18@gmail.com>
2025-02-05 03:35:24 -0500

    feat: compress the weight and type of imgs

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,9 +2,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import couple1 from "../img/couple1.jpg"
-import couple2 from "../img/couple2.jpg"
-import couple3 from "../img/couple3.jpg"
+import couple1 from "../img/couple1.webp"
+import couple2 from "../img/couple2.webp"
+import couple3 from "../img/couple3.webp"
 const MyCarousel = () => {
     const settings = { dots: true, infinite: true, speed: 500, slidesToShow: 1,arrows:false, slidesToScroll: 1, autoplay: true, autoplaySpeed: 1500 , lazyLoad:'ondemand' };
     return (
